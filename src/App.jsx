e27df5e88deb6368547a0832231449a9d86d557a
Cesar Miranda <cesargermanmiranda@gmail.com>
2025-05-12 17:37:06 -0300

    Estructura inicial del App y ProductForm - subido por Cesar Miranda

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+import ProductForm from './components/ProductForm';
+import ProductList from './components/ProductList';
+import SearchBar from './components/SearchBar';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [editingProduct, setEditingProduct] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredProducts = useMemo(() => {
+    return products.filter(p =>
+      p.descripcion.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      p.id.toString().includes(searchTerm)
+    );
+  }, [products, searchTerm]);
+
+  const addProduct = useCallback((product) => {
+    setProducts(prev => [...prev, product]);
+  }, []);
+
+  const updateProduct = useCallback((updatedProduct) => {
+    setProducts(prev => prev.map(p => (p.id === updatedProduct.id ? updatedProduct : p)));
+    setEditingProduct(null);
+  }, []);
+
+
+  const deleteProduct = useCallback((id) => {
+    setProducts(prev => prev.filter(p => p.id !== id));
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h2>Gestión de Productos - PV2025 - Grupo 16</h2>
+      <p> Buscar: </p>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+
+      <ProductForm
+        addProduct={addProduct}
+        updateProduct={updateProduct}
+        editingProduct={editingProduct}
+      />
+      <ProductList
+        products={filteredProducts}
+        setEditingProduct={setEditingProduct}
+        deleteProduct={deleteProduct}
+      />
+    </div>
+  );
+
 }
 
-export default App
+export default App;
